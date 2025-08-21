cbeea47fdb8c777f137bcd9c42cd41523642c3d2
syed abdul muqeeth <samx123786@gmail.com>
2025-08-21 12:08:38 +0530

    added theme toggler from magicui

--- app/providers/ConvexClientProvider.tsx
+++ app/providers/ConvexClientProvider.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
 export default function ConvexClientProvider({
   children,
 }: {
